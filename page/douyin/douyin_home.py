0db3eab62026cd97b861896600654655219de85a
zhubaojia@123 <1327549487@qq.com>
2023-02-22 14:49:43 +0800

    抖音登陆

--- page/douyin/douyin_home.py
+++ page/douyin/douyin_home.py
@@ -5,14 +5,20 @@ from base.base_action import BaseAction
 
 class DyPage(BaseAction):
     # ------ 元素的特征 ------
-    # help_center_button = By.LINK_TEXT, "帮助中心"
+    # 百度搜索输入框
     dy_feature = By.XPATH, '//*[@id="kw"]'
-    # baidu_search = By.ID, 'su'
-    baidu_search = By.XPATH, '//*[@id="su"]'
+    # 百度搜索按钮
+    baidu_search = By.ID, 'su'
+    # 点击抖音首页
     dy_home_feature = By.XPATH, '//*[@id="1"]/div/div[1]/h3/a[1]'
-    # ------ 对元素的操作 ------
-    # def click_help_center(self):
-    #     self.click(self.help_center_button)
+    # 点击账户密码登陆
+    dy_login = By.XPATH, '//*[@id="web-login-container"]/article/article/article/div[1]/ul[1]/li[3]'
+    # 手机号输入框
+    du_login_input_mobile = By.XPATH, '//*[@id="web-login-container"]/article/article/article/form/div[1]/div/input'
+    # 密码输入框
+    du_login_input_password = By.XPATH, '//*[@id="web-login-container"]/article/article/article/form/div[2]/div/div/input'
+    # 登陆按钮
+    du_login_button = By.XPATH, '//*[@id="web-login-container"]/article/article/article/form/div[5]/button'
 
     def get_first_dy_text(self):
         return self.input(self.dy_feature, "抖音")
@@ -22,3 +28,15 @@ class DyPage(BaseAction):
 
     def get_first_dy_home_text(self):
         return self.click(self.dy_home_feature)
+
+    def get_first_dy_login_text(self):
+        return self.click(self.dy_login)
+
+    def get_first_dy_mobile(self):
+        return self.input(self.du_login_input_mobile, "17610115330")
+
+    def get_first_dy_password(self):
+        return self.input(self.du_login_input_password, "Zhubaojia123")
+
+    def get_first_dy_login_button(self):
+        return self.click(self.du_login_button)
--- scripts/test_help.py
+++ scripts/test_help.py
@@ -21,17 +21,25 @@ class TestHelp(unittest.TestCase):
         self.driver.quit()
 
     def test_dy_home_center(self):
-        # 百度搜索抖音 - 点击首页
+        # 百度搜索抖音
         self.dy_home_page.get_first_dy_text()
+        # 点击搜索按钮
         self.dy_home_page.get_first_baidu_search_text()
+        # 点击第一个抖音首页
         self.dy_home_page.get_first_dy_home_text()
-
+        # 切换窗口
         self.driver.switch_to.window(self.driver.window_handles[-1])
-
+        # 点击登陆页面
+        self.dy_home_page.get_first_dy_login_text()
+        # 输入手机号
+        self.dy_home_page.get_first_dy_mobile()
+        self.dy_home_page.get_first_dy_password()
+        self.dy_home_page.get_first_dy_login_button()
+        # 输入密码
+        # 点击登陆
         # 帮助页面 - 获取第一个热词
         # word = self.help_page.get_first_hotword_text()
         word = self.driver.current_url
-        print("111111111111", word)
 
         # 断言： 第一个热词是不是叫做"百度账号被封禁"
         assert "douyin" in word
